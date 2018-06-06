811634b4d77b043f98c15056f54be633a97f5009
JB <jb@boyce.com.au>
2018-06-07 09:52:03 +1000

    Updated Angular framework from 5 to 6.

--- ClientApp/src/app/http.interceptor.ts
+++ ClientApp/src/app/http.interceptor.ts
@@ -1,6 +1,6 @@
 ﻿import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from "@angular/common/http";
-import { Observable } from "rxjs/Observable";
+import { Observable } from "rxjs";
  
 @Injectable()
 export class HttpInterceptorClass implements HttpInterceptor {
